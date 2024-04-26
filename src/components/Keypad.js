2e2d6db1f87e972e32e396ebc971c79a7a7c0058
Emmanuel Joel Odhiambo <joel.emmanuel1968@gmail.com>
2024-04-26 13:47:04 +0300

    lab completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
 // Code Keypad Component Here
-
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from "react";
+function Keypad() {
+  const change = () => {
+    console.log("Entering password...");
+  };
+  return (
+    <div>
+      <input type="password" onChange={change}></input>
+    </div>
+  );
 }
 
 export default Keypad;
